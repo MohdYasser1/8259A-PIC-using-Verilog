d2e5968a0097cffdcf4891a890330145df45c01c
a7med31 <ahmed.abdulrahman3000@gmail.com>
2023-12-31 21:36:15 +0200

    Update control module

--- CONTROL.v
+++ CONTROL.v
@@ -2,22 +2,37 @@
 module ControlLogic (
   input wire SP,          //Master SP=1  slave SP = 0
   input wire INTA,
-  input wire [2:0] INT_VEC, //
+  input wire [2:0] INT_VEC, //from pirority resolver
 
   input wire [3:0]ICWs_Flags, //from read write logic
   input wire [2:0]OCWs_Flags, //from read write logic
 
   input wire [7:0] DATA_IN,  // Data bus from other blocks
-  output wire [7:0] IV,      // Interrupt Vector
 
   inout wire [2:0] CAS,     // Cascade Signals
-  output reg [7:0] IMR,      // Interrupt Mask Register
+
+  input wire [7:0] IR, // Interrupt Request lines connected to I/O
+  
+  output wire [7:0] IM,      // Interrupt Mask Register
   output wire [1:0]Read_command,
   output wire AEOI,
   output wire LTIM,
-  output wire [7:0]opperation_OCW2
+  output wire [7:0]opperation_OCW2,
+  
+
+  //////////////
+  output wire [7:0] IV,      // Interrupt Vector
+  output wire first_ACK ,
+  output wire second_ACK,
+  output wire IV_ready
+
 
 );
+
+//////////////////////////////////////
+reg[7:0] IRR_masked;
+wire int_from_slave ;
+//////////////////////////////////////
 
 //////////////////wires declaration////////////////////////////////
 wire  ICW4 ,SNGL,SFNM ,BUF, M_S ,POLL, cascade_slave,cascade_mode;
@@ -25,9 +40,11 @@ wire  ICW1_RECEIVED, ICW2_RECEIVED, ICW3_RECEIVED, ICW4_RECEIVED,OCW1_RECEIVED,O
 wire [4:0] T7_T3;
 wire [1:0]Special_Mask_Mode;
 
-reg [7:0]DATA_OUT;
+reg [7:0] DATA_OUT;
+reg [7:0] IMR;
 
-reg [2:0] CAS_IN,CAS_OUT;
+reg [2:0] CAS_IN;
+reg [2:0] CAS_OUT;
 assign CAS = CAS_OUT;
 
   ///////////////
@@ -145,24 +162,86 @@ end
     end
   end
 
+ assign IM = IMR;
 
+ //cascade mode
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+  always @(IR,IMR) begin
+        IRR_masked <= IR & ~IMR;
+  end
+
+reg send_IV;
 assign cascade_salve = SNGL?0:(SP?0:0); // if no cascade then 0 if cascade_mode 0 for master and 1 for slave
 assign cascade_mode = SNGL?0:1;          //1 if cascade mode is on
+
+assign int_from_slave = !SNGL ? (IRR_masked & icw3) != 8'b00000000 : 0; 
+
+always @(CAS,cascade_mode,cascade_salve) begin
+  if(cascade_slave && cascade_mode)begin
+    CAS_IN<=CAS;
+  end 
+end
+
+always @(cascade_mode,cascade_slave,int_from_slave,first_ACK,second_ACK,INT_VEC) begin
+ if(cascade_mode) begin            // if cascade mode is on
+   if (!cascade_salve) begin      // if master
+    if(int_from_slave && (second_ACK)) begin // if slave active and during INTA 2 pulses cas_out = interupt vector
+      CAS_OUT <= INT_VEC;
+      send_IV <=0;
+    end else if(!int_from_slave) begin  // if master but slave not active there is a device connected to this interrupt line
+       DATA_OUT <= {T7_T3, INT_VEC};
+       send_IV <=1;
+    end
+   end else begin                      // if slave
+      if (second_ACK && (icw3[2:0]==CAS_IN[2:0])) begin // if cas lines equal slave address and second ack recieved 
+      DATA_OUT <= {T7_T3, INT_VEC};   
+      send_IV<=1;           
+     end
+   end
+ end else begin
+  if(second_ACK) begin  // if single mode and secon ACK is Received
+      DATA_OUT <= {T7_T3, INT_VEC};
+      send_IV<=1;
+  end
+ end  
+end
+
+assign IV_ready = send_IV && second_ACK;  
+assign CAS = CAS_OUT;
+assign IV[7:0] = DATA_OUT[7:0];
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 reg[1:0] control_state ;
 reg[1:0] next_control_state ;
 
-/////////////////////////////////
+reg negedge_INTA =0;
+reg [1:0 ]INTA_pulses=2'b00;
+
+
+always@(negedge INTA) begin
+  //negedge_INTA = 1;
+  INTA_pulses = INTA_pulses + 1;
+end
+
+always@(posedge INTA) begin
+  if (INTA_pulses == 2'b10)begin
+    INTA_pulses = 2'b00;
+  end
+end
 
 
 always @(next_control_state) begin
   control_state = next_control_state ;
 end
 
-always @(control_state,INTA) begin
+always @(control_state,negedge INTA,INTA_pulses) begin
  case (control_state)
   CTL_READY : begin
-    if (INTA==0) begin
+    if (INTA_pulses == 2'b01 ) begin
       next_control_state = ACK1;
     end else begin
       next_control_state = CTL_READY ;
@@ -170,7 +249,7 @@ always @(control_state,INTA) begin
     
   end 
   ACK1:begin
-    if(INTA==0) begin
+    if(INTA_pulses == 2'b10) begin
       next_control_state = ACK2;
     end else begin
       next_control_state =ACK1;
@@ -178,24 +257,62 @@ always @(control_state,INTA) begin
     
   end
   ACK2:begin
-    if(!cascade_mode)begin
-      DATA_OUT = {T7_T3, INT_VEC};
+    if(INTA_pulses == 2'b00)begin
+        next_control_state= CTL_READY; 
+    end else begin
+      next_control_state =ACK2;
     end
-    next_control_state= CTL_READY;  
   end
   default: begin
   control_state= CTL_READY; 
   end
  endcase
 end
+
+assign first_ACK = (control_state == ACK1);
+assign second_ACK = (control_state == ACK2);
+
+//////Interrupt //////////
+   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-always @(CAS) begin
+  // set the interrupt line to 1 if there is an interrupt request that is not masked
+  
+    assign INT = INT_REG;
+    
+    // when the Interrupt Request lines changes update internal IRR
+    always @(IR) begin
+        IRR <= IR;
+    end
 
-  if(!SNGL && cascade_slave)begin //if cascade mode and Slave
-    CAS_IN <= CAS;
-  end
- /*if(!SNGL && !cascade_salve)begin //if cascade mode and master
-  end
-end*/
+    always @(IRR,IMR) begin
+        IRR_masked <= IRR & ~IMR;
+    end
 
-endmodule
+always @(IRR_masked or LTIM) begin
+    // Level-triggered logic
+    if (LTIM) begin
+        if (IRR_masked != 8'b0) begin
+            // Any high input generates an interrupt
+            INT_REG <= 1;
+        end else begin
+            // No interrupt request
+            INT_REG <= 0;
+        end
+    end else begin
+        // Edge-triggered logic
+        if (IRR_masked != request_latch) begin
+            // Transition occurred, generate interrupt
+            INT_REG <= 1;
+        end else begin
+            // No transition, no interrupt
+            INT_REG <= 0;
+        end
+    end
+
+    // Request latch (transparent D-type latch)
+    request_latch <= IRR_masked;
+end
+*/
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    endmodule
