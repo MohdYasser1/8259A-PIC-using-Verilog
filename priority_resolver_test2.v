d92ba2f336a9f63dacd68da358d1926011606ac6
Abdulrahman295 <movieshome160@gmail.com>
2024-01-03 19:23:27 +0200

    test2.1

--- priority_resolver_test2.v
+++ priority_resolver_test2.v
@@ -30,15 +30,115 @@ module testbench;
     );
 
     initial begin
-        AEOI = 0;
         LTIM = 1;
-        //// test(1): fixed priority mode and non-specific end of interrupt////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // test(1): automatic rotating priority mode with Automatic End of Interrup
+        AEOI = 1;
+        IR = 8'b01001000;
+        IM = 8'b00000000;
+        operation = 8'b10000000;
+        $display("//// test(1): automatic rotating priority mode with Automatic End of Interrup: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+        
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        #15
+        $display("send first pulse");
+        INTA = 1;
+        #5
+        INTA = 0;
+
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        $display("send second pulse");
+        $display("automatic end of interrupt: AEOI: %b", AEOI);
+        INTA = 1;
+        #5
+        INTA = 0;
+        
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        IR = 8'b01001001;
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        #15
+        $display("send first pulse");
+        INTA = 1;
+        #5
+        INTA = 0;
+
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        $display("send second pulse");
+        $display("automatic end of interrupt: AEOI: %b", AEOI);
+        INTA = 1;
+        #5
+        INTA = 0;
+        
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        
+        #15
+        $display("send first pulse");
+        INTA = 1;
+        #5
+        INTA = 0;
+
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        $display("send second pulse");
+        $display("automatic end of interrupt: AEOI: %b", AEOI);
+        INTA = 1;
+        #5
+        INTA = 0;
+        
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        
+        #15
+        $display("send first pulse");
+        INTA = 1;
+        #5
+        INTA = 0;
+
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        $display("send second pulse");
+        $display("automatic end of interrupt: AEOI: %b", AEOI);
+        INTA = 1;
+        #5
+        INTA = 0;
+        
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        operation = 8'b00000000; // reset operation
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //// test(2): fixed priority mode and non-specific end of interrupt////
+        AEOI = 0;
         IR = 8'b10000001;
         IM = 8'b00000000;
         operation = 8'b01000000;
-        $display("//// test(1): fixed priority mode and non-specific end of interrupt: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+        $display("//// test(2): fixed priority mode and non-specific end of interrupt: IR= %b, IM= %b, operation= %b", IR, IM, operation);
         
-         #10
+        #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
         
         $display("send first pulse");
@@ -66,6 +166,40 @@ module testbench;
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
         operation = 8'b00000000; // reset operation
+        
+        IR = 8'b10000100;
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        
+        
+        // repeat for IR2
+        $display("send first pulse");
+        INTA = 1;
+        #5
+        INTA = 0;
+
+        // Wait for some time and display result
+        #10;
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        $display("send second pulse");
+        INTA = 1;
+        #5
+        INTA = 0;
+        
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        
+        operation = 8'b00100000;
+        $display("issue non-specific end of interrupt");
+        
+        // Wait for some time and display result
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        operation = 8'b00000000; // reset operation
+        
         
         // repeat for IR7
         $display("send first pulse");
@@ -95,14 +229,14 @@ module testbench;
         operation = 8'b00000000; // reset operation
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         
-         //// test(2): rotating priority mode and specific end of interrupt ////
+         //// test(3): specific rotating priority mode and specific end of interrupt ////
          IR = 8'b00110001;
          IM = 8'b00000000;
          operation = 8'b11000011; // set the highest priority to 4
-         $display("//// test(2): rotating priority mode and specific end of interrupt: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+         $display("//// test(3): specific rotating priority mode and specific end of interrupt: IR= %b, IM= %b, operation= %b", IR, IM, operation);
          
          #10
-        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
          
          $display("send first pulse");
          INTA = 1;
@@ -124,18 +258,16 @@ module testbench;
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
         
+        operation = 8'b00000000;
+        #5
         operation = 8'b00100000;
         $display("issue non-specific end of interrupt");
         
         // Wait for some time and display result
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
-        operation = 8'b10000000; // Automatic rotating operation ****
-        IR = 8'b00000000;
-        #5
-        IR = 8'b00110001;
-        #10
-        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
+        //operation = 8'b10000000; // Automatic rooperation = 8'b10000000; // Automatic rotating operation ****
+        
         
         // repeat for IR5
         $display("send first pulse");
@@ -164,7 +296,7 @@ module testbench;
         // Wait for some time and display result
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
-        operation = 8'b10000000; // Automatic rotating operation
+        //operation = 8'b10000000; // Automatic rotating operation
         
         // repeat for IR0
         
@@ -188,21 +320,20 @@ module testbench;
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
         
         
-        operation = 8'b01100101;
-        $display("issue specific end of interrupt for interrupt 5");
+        operation = 8'b01100000;
+        $display("issue specific end of interrupt for interrupt 0");
         
         // Wait for some time and display result
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
-        IR = 8'b00000000;
         operation = 8'b00000000;
         #10
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // test(3): Interrupt Masking
+        // test(4): Interrupt Masking
         IR = 8'b00000001;
         IM = 8'b00000001;
         operation = 8'b01000000;
-        $display("//// test(3): Interrupt Masking: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+        $display("//// test(4): Interrupt Masking: IR= %b, IM= %b, operation= %b", IR, IM, operation);
         
         // Wait for some time and display result
         #10
@@ -244,11 +375,11 @@ module testbench;
         IR = 8'b00000000;
         #10
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // test(4): Automatic End of Interrup
+        // test(5): Automatic End of Interrup
         IR = 8'b00001000;
         IM = 8'b00000000;
         operation = 8'b00000000;
-        $display("//// test(4): Automatic End of Interrup: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+        $display("//// test(5): Automatic End of Interrup: IR= %b, IM= %b, operation= %b", IR, IM, operation);
         
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
@@ -275,11 +406,13 @@ module testbench;
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
         
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // test(5): Edge trigger with Automatic End of Interrup
+        // test(6): Edge trigger with Automatic End of Interrup
         IR = 8'b00000000;
         IM = 8'b00000000;
         operation = 8'b00000000;
-        $display("//// test(5): Edge trigger with Automatic End of Interrup: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+        $display("//// test(6): Edge trigger with Automatic End of Interrup: IR= %b, IM= %b, operation= %b", IR, IM, operation);
+        #10
+        $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
         
         LTIM = 0;
         AEOI = 1;
@@ -314,6 +447,6 @@ module testbench;
         // Wait for some time and display result
         #10
         $display("INT: %b, INT_VEC: %b, ISR: %b, IRR: %b, IMR: %b", INT, INT_VEC, ISR, IRR, IMR);
-        
-    end
+
+        end
 endmodule
